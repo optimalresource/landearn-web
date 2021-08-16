9c40b5093e9547e27f3d6890a4ee58f7454c8ee2
optimalresource <joptimalresource@live.com>
2021-08-16 01:34:30 +0100

    Just uploading a skeleton design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import TopHeader from './components/header/top/';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TopHeader />
   </React.StrictMode>,
   document.getElementById('root')
 );

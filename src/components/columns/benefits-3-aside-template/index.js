7e8fe7cf42a7397646d1af608ab10aa472a8c9a9
optimalresource <joptimalresource@live.com>
2021-08-23 11:35:55 +0100

    Good job...................

--- src/components/columns/benefits-3-aside-template/index.js
+++ src/components/columns/benefits-3-aside-template/index.js
@@ -1,20 +1,53 @@
 import React from 'react'
 import './index.css'
+import LegalIcon from '../../../assets/images/mobile/legal.svg'
+import VerifiedIcon from '../../../assets/images/mobile/verified.svg'
+import SupportIcon from '../../../assets/images/mobile/support.svg'
 
 const ThreeAside = () => {
     return (
-        <div class="benefits border-1px-dark300">
-            <div class="frame-19">
-            <img
-                class="iconssupportagentblack24dp"
-                alt="icon"
-                src="https://anima-uploads.s3.amazonaws.com/projects/611bfeda331562400fadc1b1/releases/611c37a298305a35d443c704/img/icons-support-agent-black-24dp@2x.png"
-            />
-            <div class="text-10 axiforma-bold-amazon-18px">Free support</div>
+        <div className="benefits_wrap">
+            <div className="benefits border-1px-dark300">
+                <div className="frame-19">
+                <img
+                    className="iconssupportagentblack24dp"
+                    alt="icon"
+                    src={SupportIcon}
+                />
+                <div className="text-10 axiforma-bold-amazon-18px">Free support</div>
+                </div>
+                <p className="text-11 axiforma-regular-normal-black-14px">
+                    One year support on your home inspection, enquiry or issues.
+                </p>
             </div>
-            <p class="text-11 axiforma-regular-normal-black-14px">
-                One year support on your home inspection, enquiry or issues.
-            </p>
+
+            <div className="benefits border-1px-dark300">
+                <div className="frame-19">
+                <img
+                    className="iconssupportagentblack24dp"
+                    alt="icon"
+                    src={VerifiedIcon}
+                />
+                <div className="text-10 axiforma-bold-amazon-18px">Verified offers</div>
+                </div>
+                <p className="text-11 axiforma-regular-normal-black-14px">
+                    Properties are of valid titles and information stated is as is.
+                </p>
+            </div>
+
+            <div className="benefits border-1px-dark300">
+                <div className="frame-19">
+                <img
+                    className="iconssupportagentblack24dp"
+                    alt="icon"
+                    src={LegalIcon}
+                />
+                <div className="text-10 axiforma-bold-amazon-18px">Legal help</div>
+                </div>
+                <p className="text-11 axiforma-regular-normal-black-14px">
+                    Documents are prepared for you by Landearn legal team.
+                </p>
+            </div>
         </div>
     )
 }

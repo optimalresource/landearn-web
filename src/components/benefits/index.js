dd33e39e0c543eed2e875577eaef0339c8bf529b
optimalresource <joptimalresource@live.com>
2021-08-23 02:24:23 +0100

    Good job..............

--- src/components/benefits/index.js
+++ src/components/benefits/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './index.css'
 import Tab from '../switchable-tab'
+import ThreeAside from '../columns/benefits-3-aside-template'
 
 const Benefits = () => {
     return (
         <div className="benefit-wrapper">
             <div className="text axiforma-bold-cod-gray-28px" data-id="70:1998">Some key benefits</div>
             <Tab />
+            <ThreeAside />
         </div>
     )
 }
---
+++ src/components/columns/benefits-3-aside-template/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+
+const ThreeAside = () => {
+    return (
+        <div class="benefits border-1px-dark300">
+            <div class="frame-19">
+            <img
+                class="iconssupportagentblack24dp"
+                src="https://anima-uploads.s3.amazonaws.com/projects/611bfeda331562400fadc1b1/releases/611c37a298305a35d443c704/img/icons-support-agent-black-24dp@2x.png"
+            />
+            <div class="text-10 axiforma-bold-amazon-18px">Free support</div>
+            </div>
+            <p class="text-11 axiforma-regular-normal-black-14px">
+                One year support on your home inspection, enquiry or issues.
+            </p>
+        </div>
+    )
+}
+
+export default ThreeAside;

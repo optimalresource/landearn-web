624341f15f3d901f1e6eede55d31adb7b18b01bd
optimalresource <joptimalresource@live.com>
2021-08-23 01:19:48 +0100

    Good job.........

---
+++ src/components/benefits/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+import Tab from '../switchable-tab'
+
+const Benefits = () => {
+    return (
+        <Tab />
+    )
+}
+
+export default Benefits;

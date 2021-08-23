23961829cce5c7a8a7a73f402faf7cb2183a152a
optimalresource <joptimalresource@live.com>
2021-08-23 10:32:52 +0100

    Good job.................

--- src/components/switchable-tab/index.js
+++ src/components/switchable-tab/index.js
@@ -5,11 +5,11 @@ const Tab = () => {
     return (
         <div class="x712778">
             <div class="frame-17-fxmCFR">
-                <div class="frame-16-NPPK8P">
-                <div class="text-8-Bx4pEd">For buyers</div>
+                <div class="frame-16-NPPK8P active">
+                    <div class="text-8-Bx4pEd ">For buyers</div>
                 </div>
-                <div class="frame-15-NPPK8P">
-                <div class="text-9-T9vfNx">For sellers</div>
+                <div class="frame-15-NPPK8P inactive">
+                    <div class="text-9-T9vfNx">For sellers</div>
                 </div>
             </div>
         </div>

c0a596b4406d6d74a620382bc69e1ec569941393
optimalresource <joptimalresource@live.com>
2021-08-23 01:11:19 +0100

    Good job........

--- src/components/switchable-tab/index.js
+++ src/components/switchable-tab/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Tab = () => {
     return (
-        <div>
-            Hello
+        <div class="x712778">
+            <div class="frame-17-fxmCFR">
+                <div class="frame-16-NPPK8P">
+                <div class="text-8-Bx4pEd">For buyers</div>
+                </div>
+                <div class="frame-15-NPPK8P">
+                <div class="text-9-T9vfNx">For sellers</div>
+                </div>
+            </div>
         </div>
     )
 }
